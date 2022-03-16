89c399d3c898806c1d1e9b479c1d4a7149f90e7c
Daniel Hornik <daniel.1302@gmail.com>
2022-03-16 10:40:12 +0100

    Refactor files for state cmd

--- cmd/state.go
+++ cmd/state.go
@@ -1,6 +1,9 @@
 package cmd
 
 import (
+	"fmt"
+
+	"code.vegaprotocol.io/vegacapsule/state"
 	"github.com/spf13/cobra"
 )
 
@@ -10,5 +13,24 @@ var stateCmd = &cobra.Command{
 }
 
 func init() {
-	stateCmd.AddCommand(stateGetAddresses)
+	stateCmd.AddCommand(stateGetSmartcontractsAddresses)
+}
+
+var stateGetSmartcontractsAddresses = &cobra.Command{
+	Use:   "get-smartcontracts-addresses",
+	Short: "Print smartcontracts addresses and keys passed to vegacapsule as a config parameter",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		netState, err := state.LoadNetworkState(homePath)
+		if err != nil {
+			return err
+		}
+
+		if netState.Empty() {
+			return networkNotBootstrappedErr("state get-smartcontracts-addresses")
+		}
+
+		fmt.Println(netState.Config.Network.SmartContractsAddresses)
+
+		return nil
+	},
 }
