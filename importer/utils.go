b11a5a17bbad508027e741135aa951a41b2ef475
Daniel Hornik <daniel.1302@gmail.com>
2022-06-27 15:07:12 -0500

    feat: cleanup required data for import command

--- importer/utils.go
+++ importer/utils.go
@@ -1,9 +1,35 @@
 package importer
 
 import (
+	"encoding/base64"
 	"fmt"
 	"io/ioutil"
+
+	"github.com/tendermint/tendermint/crypto/ed25519"
 )
+
+type tendermintKey struct {
+	PrivateKey string
+	PublicKey  string
+	NodeID     string
+}
+
+func decodeTendermintPrivateKey(privateKey string) (*tendermintKey, error) {
+	var privKey ed25519.PrivKey
+
+	privKey, err := base64.StdEncoding.DecodeString(privateKey)
+	if err != nil {
+		return nil, fmt.Errorf("failed to decode private key %s, %w", privateKey, err)
+	}
+	publicKey := base64.StdEncoding.EncodeToString(privKey.PubKey().Bytes())
+	nodeId := privKey.PubKey().Address().String()
+
+	return &tendermintKey{
+		PrivateKey: privateKey,
+		PublicKey:  publicKey,
+		NodeID:     nodeId,
+	}, nil
+}
 
 func createTempFile(content string) (string, error) {
 	file, err := ioutil.TempFile("", "vegacapsule_import")
