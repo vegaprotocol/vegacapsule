85c07416970e9420e181685cbf916114a827f6bc
Karel Moravec <moravec.wdd@gmail.com>
2022-10-12 20:48:28 +0200

    feat: remove template flags

--- config/config.go
+++ config/config.go
@@ -96,12 +96,12 @@ type DockerConfig struct {
 	Name         string            `hcl:"name,label"`
 	Image        string            `hcl:"image"`
 	Command      string            `hcl:"cmd,optional"`
-	Args         []string          `hcl:"args" template:""`
+	Args         []string          `hcl:"args"`
 	Env          map[string]string `hcl:"env,optional"`
 	StaticPort   *StaticPort       `hcl:"static_port,block"`
 	AuthSoftFail bool              `hcl:"auth_soft_fail,optional"`
 	Resources    *Resources        `hcl:"resources,block"`
-	VolumeMounts []string          `hcl:"volume_mounts,optional" template:""`
+	VolumeMounts []string          `hcl:"volume_mounts,optional"`
 }
 
 type WalletConfig struct {
