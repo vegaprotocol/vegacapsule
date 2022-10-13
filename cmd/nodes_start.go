cfe5fb9667a3fe9add8a39809b8a22c537b47e78
Karel Moravec <moravec.wdd@gmail.com>
2022-10-13 15:44:41 +0200

    feat: add multiple nodes at the same time

--- cmd/nodes_start.go
+++ cmd/nodes_start.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 
+	"code.vegaprotocol.io/vegacapsule/config"
 	"code.vegaprotocol.io/vegacapsule/nomad"
 	"code.vegaprotocol.io/vegacapsule/state"
 	"code.vegaprotocol.io/vegacapsule/types"
@@ -30,12 +31,24 @@ var nodesStartCmd = &cobra.Command{
 			return networkNotBootstrappedErr("nodes start")
 		}
 
-		updatedNetworkState, err := nodesStartNode(context.Background(), *networkState, nodeName, vegaBinary)
+		nodeSet, err := networkState.GeneratedServices.GetNodeSet(nodeName)
+		if err != nil {
+			return err
+		}
+
+		nomadJobID, err := nodesStartNode(
+			context.Background(),
+			nodeSet,
+			networkState.Config,
+			vegaBinary,
+		)
 		if err != nil {
 			return fmt.Errorf("failed start node: %w", err)
 		}
 
-		return updatedNetworkState.Persist()
+		networkState.RunningJobs.NodesSetsJobIDs[nomadJobID] = true
+
+		return networkState.Persist()
 	},
 }
 
@@ -53,32 +66,27 @@ func init() {
 	nodesStartCmd.MarkFlagRequired("name")
 }
 
-func nodesStartNode(ctx context.Context, state state.NetworkState, name, vegaBinary string) (*state.NetworkState, error) {
-	log.Printf("starting %s node set", name)
-
-	nodeSet, err := state.GeneratedServices.GetNodeSet(name)
-	if err != nil {
-		return nil, err
-	}
+func nodesStartNode(ctx context.Context, nodeSet *types.NodeSet, conf *config.Config, vegaBinary string) (string, error) {
+	log.Printf("starting %s node set", nodeSet.Name)
 
 	nomadClient, err := nomad.NewClient(nil)
 	if err != nil {
-		return nil, fmt.Errorf("failed to create nomad client: %w", err)
+		return "", fmt.Errorf("failed to create nomad client: %w", err)
 	}
 
-	nomadRunner, err := nomad.NewJobRunner(nomadClient, *state.Config.VegaCapsuleBinary, state.Config.LogsDir())
+	nomadRunner, err := nomad.NewJobRunner(nomadClient, *conf.VegaCapsuleBinary, conf.LogsDir())
 	if err != nil {
-		return nil, fmt.Errorf("failed to create job runner: %w", err)
+		return "", fmt.Errorf("failed to create job runner: %w", err)
 	}
 
 	if _, err := nomadRunner.RunRawNomadJobs(ctx, nodeSet.PreGenerateRawJobs()); err != nil {
-		return nil, fmt.Errorf("failed to start node set %q pre generate jobs: %w", nodeSet.Name, err)
+		return "", fmt.Errorf("failed to start node set %q pre generate jobs: %w", nodeSet.Name, err)
 	}
 
 	if vegaBinary != "" {
 		vegaBinPath, err := utils.BinaryAbsPath(vegaBinary)
 		if err != nil {
-			return nil, fmt.Errorf("failed to get absolute path for %q: %w", vegaBinary, err)
+			return "", fmt.Errorf("failed to get absolute path for %q: %w", vegaBinary, err)
 		}
 
 		nodeSet.Vega.BinaryPath = vegaBinPath
@@ -90,12 +98,9 @@ func nodesStartNode(ctx context.Context, state state.NetworkState, name, vegaBin
 
 	res, err := nomadRunner.RunNodeSets(ctx, []types.NodeSet{*nodeSet})
 	if err != nil {
-		return nil, fmt.Errorf("failed to start nomad node set %q : %w", nodeSet.Name, err)
+		return "", fmt.Errorf("failed to start nomad node set %q : %w", nodeSet.Name, err)
 	}
 
-	state.RunningJobs.NodesSetsJobIDs[*res[0].ID] = true
-	state.GeneratedServices.NodeSets[nodeSet.Name] = *nodeSet
-
-	log.Printf("starting %s node set success", name)
-	return &state, nil
+	log.Printf("starting %s node set success", nodeSet.Name)
+	return *res[0].ID, nil
 }
