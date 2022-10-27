6603fb4f3c306527ad694b5aac32533ae56e1515
Daniel <daniel.1302@gmail.com>
2022-10-27 06:56:08 -0500

    Revert "Revert "feat: use raw template from config (#300)" (#302)"
    This reverts commit 7e8857883353b6cac4457a62adfe6f3f658ffd68.

--- config/parser.go
+++ config/parser.go
@@ -40,9 +40,13 @@ var envFunc = function.New(&function.Spec{
 	},
 })
 
-func newEvalContext(genServices cty.Value) *hcl.EvalContext {
+func newEvalContext(genServices cty.Value, homePath string) *hcl.EvalContext {
+
 	return &hcl.EvalContext{
-		Variables: map[string]cty.Value{"generated": genServices},
+		Variables: map[string]cty.Value{
+			"generated":         genServices,
+			"network_home_path": cty.StringVal(homePath),
+		},
 		Functions: map[string]function.Function{
 			"abs":             stdlib.AbsoluteFunc,
 			"base64decode":    encoding.Base64DecodeFunc,
@@ -122,7 +126,7 @@ func ApplyConfigContext(conf *Config, genServices *types.GeneratedServices) (*Co
 		return nil, fmt.Errorf("failed to convert GeneratedServices to cty value: %w", err)
 	}
 
-	if err := hclsimple.Decode("config.hcl", conf.HCLBody, newEvalContext(*genServicesCtyVal), conf); err != nil {
+	if err := hclsimple.Decode("config.hcl", conf.HCLBodyRaw, newEvalContext(*genServicesCtyVal, *conf.OutputDir), conf); err != nil {
 		return nil, err
 	}
 
@@ -164,7 +168,13 @@ func ParseConfigFile(filePath, outputDir string, genServices types.GeneratedServ
 		return nil, err
 	}
 
-	decodeDiags := gohcl.DecodeBody(f.Body, newEvalContext(*genServicesCtyVal), config)
+	configContent, err := os.ReadFile(filePath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to load file from disk: %w", err)
+	}
+	config.HCLBodyRaw = configContent
+
+	decodeDiags := gohcl.DecodeBody(f.Body, newEvalContext(*genServicesCtyVal, *config.OutputDir), config)
 	if decodeDiags.HasErrors() {
 		return nil, fmt.Errorf("failed to decode config: %w", decodeDiags)
 	}
