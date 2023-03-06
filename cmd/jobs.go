cf6ac4951fee48c7067db5769306bf2a9b48aef4
Karel Moravec <moravec.wdd@gmail.com>
2023-03-06 21:09:21 +0100

    Update cmd/jobs.go
    Co-authored-by: wwestgarth <william@vega.xyz>

--- cmd/jobs.go
+++ cmd/jobs.go
@@ -4,7 +4,7 @@ import "github.com/spf13/cobra"
 
 var jobsCmd = &cobra.Command{
 	Use:   "jobs",
-	Short: "Allos to start/stop a specific jobs",
+	Short: "Allows to start/stop specific jobs",
 	Long:  "The command allows some operations on a specific job.",
 }
 
