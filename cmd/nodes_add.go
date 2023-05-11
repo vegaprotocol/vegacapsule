a5f37708166ede844681cd4c6e9ab0076fc7d9f2
daniel1302 <daniel.1302@gmail.com>
2023-05-11 06:14:14 +0200

    feat: implement feature to add node into group without existing one

--- cmd/nodes_add.go
+++ cmd/nodes_add.go
@@ -16,6 +16,7 @@ import (
 
 var (
 	baseOnNode     string
+	baseOnGroup    string
 	startNode      bool
 	resultsOutPath string
 	count          int
@@ -45,7 +46,7 @@ var nodesAddCmd = &cobra.Command{
 		for i := 0; i < count; i++ {
 			i := i + 1
 			eg.Go(func() error {
-				newNodeSet, err := nodesAddNode(*networkState, i, baseOnNode)
+				newNodeSet, err := nodesAddNode(*networkState, i, baseOnNode, baseOnGroup)
 				if err != nil {
 					return fmt.Errorf("failed to add new node: %w", err)
 				}
@@ -108,6 +109,11 @@ func init() {
 		"",
 		"Name of the node set that the new node set should be based on",
 	)
+	nodesAddCmd.PersistentFlags().StringVar(&baseOnGroup,
+		"base-on-group",
+		"",
+		"Name of the group that the new node set should be based on",
+	)
 	nodesAddCmd.MarkFlagRequired("base-on")
 
 	nodesAddCmd.PersistentFlags().IntVar(&count,
@@ -123,7 +129,11 @@ func init() {
 	)
 }
 
-func nodesAddNode(state state.NetworkState, index int, baseOnNode string) (*types.NodeSet, error) {
+func nodesAddNode(state state.NetworkState, index int, baseOnNode, baseOnGroup string) (*types.NodeSet, error) {
+	if baseOnNode != "" && baseOnGroup != "" {
+		return nil, fmt.Errorf("provide either value for --base-on or --base-on-group, not both values")
+	}
+
 	nomadClient, err := nomad.NewClient(nil)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create nomad client: %w", err)
@@ -144,7 +154,17 @@ func nodesAddNode(state state.NetworkState, index int, baseOnNode string) (*type
 		return nil, err
 	}
 
-	nodeSet, err := state.GeneratedServices.GetNodeSet(baseOnNode)
+	var nodeSet *types.NodeSet
+	if baseOnNode != "" {
+		nodeSet, err = state.GeneratedServices.GetNodeSet(baseOnNode)
+	} else {
+		indexes, err := computeNodeIndexes(state, baseOnGroup)
+		if err != nil {
+			return nil, fmt.Errorf("failed to compute indexes for new node set: %w", err)
+		}
+		nodeSet, err = gen.InitiateSingleNodeSet(baseOnGroup, indexes.abs, indexes.group, indexes.relative)
+	}
+
 	if err != nil {
 		return nil, err
 	}
@@ -169,4 +189,35 @@ func nodesAddNode(state state.NetworkState, index int, baseOnNode string) (*type
 	}
 
 	return newNodeSet, nil
+}
+
+type nsIndex struct {
+	abs      int
+	group    int
+	relative int
+}
+
+// computeNodeIndexes computes indexes to initiate new node set.
+func computeNodeIndexes(state state.NetworkState, groupName string) (*nsIndex, error) {
+	for groupIdx, group := range state.Config.Network.Nodes {
+		if group.Name != groupName {
+			continue
+		}
+
+		result := &nsIndex{
+			group:    groupIdx,
+			abs:      len(state.GeneratedServices.NodeSets),
+			relative: 0,
+		}
+
+		for _, ns := range state.GeneratedServices.NodeSets {
+			if ns.GroupName == groupName {
+				result.relative++
+			}
+		}
+
+		return result, nil
+	}
+
+	return nil, fmt.Errorf("the group for %s not found", groupName)
 }
